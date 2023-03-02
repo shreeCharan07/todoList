af9f298f3fa49f5adda15d71165256ce57801880
shreeCharan07 <55584611+shreeCharan07@users.noreply.github.com>
2023-03-02 15:35:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
-app.set('views', path.join( __dirname + '/views'));
+app.set('views', path.join( __dirname , '/views'));
 
 
 
